62b39accbf53b7d516fa850ffb9d8b667f3a9ce0
CasperKjaerhus <casperkjaerhusleth@gmail.com>
2022-11-16 17:09:32 +0100

    Just log em

--- solana/energy-trading/tests/tic-tac-toe.ts
+++ solana/energy-trading/tests/tic-tac-toe.ts
@@ -9,27 +9,12 @@ describe("energy-trading", () => {
 
   const program = anchor.workspace.EnergyTrading as Program<TicTacToe>;
 
-  it('setup game!', async() => {
+  it('setup game!', () => {
     const gameKeypair = anchor.web3.Keypair.generate();
     const playerOne = (program.provider as anchor.AnchorProvider).wallet;
     const playerTwo = anchor.web3.Keypair.generate();
-    await program.methods
-      .setupGame(playerTwo.publicKey)
-      .accounts({
-        game: gameKeypair.publicKey,
-        playerOne: playerOne.publicKey,
-      })
-      .signers([gameKeypair])
-      .rpc();
-
-    const gameState = await program.account.game.fetch(gameKeypair.publicKey);
-    expect(gameState.turn).to.equal(1);
-    expect(gameState.players)
-      .to
-      .eql([playerOne.publicKey, playerTwo.publicKey]);
-    expect(gameState.state).to.eql({ active: {} });
-    expect(gameState.board)
-      .to
-      .eql([[null,null,null],[null,null,null],[null,null,null]]);
+    console.log(gameKeypair);
+    console.log(playerOne);
+    console.log(playerTwo);
   });
 });
