9377a6d4fd42fad992ee5a61060d598d0152d758
Alexander Nesheim <alexander.nesheim@gmail.com>
2022-11-24 14:53:06 +0100

    Added class fields.

---
+++ auction/src/auction.hs
@@ -0,0 +1,11 @@
+data Participant = Buyer Demand Price Account | Seller Supply Account 
+
+newtype Demand = D Float
+newtype Supply = S Float 
+newtype Price = P Float
+newtype Account = A String 
+
+
+
+
+
--- auction/src/auction.ts
+++ auction/src/auction.ts
@@ -19,47 +19,53 @@ type Transaction = {
 // Make the Buyer and Seller arrays to properties of the class.
 newtype Demad = Num a => D a
 class Auction {
-    solve(buyers: Buyer[], sellers: Seller[]): void {
+    sellers: Seller[];
+    buyers: Buyer[];
+    
+    constructor(sellers: Seller[], buyers: Buyer[]){
+      this.sellers = sellers;
+      this.buyers = buyers;
+    }
 
-        let sortedBids = buyers.sort((buyer1, buyer2) => buyer2.price - buyer1.price);
-        let sortedSellers = sellers.sort((seller1, seller2) => seller2.supply - seller1.supply);
+    solve(): void {
+        buyers.sort((buyer1, buyer2) => buyer2.price - buyer1.price);
+        sellers.sort((seller1, seller2) => seller2.supply - seller1.supply);
 
         let totalSupply = sellers.reduce((acc, seller) => { return acc + seller.supply }, 0);
 
         let transactions: Transaction[] = [];
         let i = 0;
-
-        while (i < sortedBids.length || totalSupply != 0) {
+        while (i < buyers.length+1 || totalSupply != 0) {
+            let bid = buyers[i];
             totalSupply = sellers.reduce((acc, seller) => { return acc + seller.supply }, 0);
-            transactions = this.serveWinner(sortedBids[i], sortedSellers);
+            transactions = this.serveWinner(bid, buyers[i+1].price);
             this.handleTransactions(transactions);
+            buyers.splice(i, 1);
         }
         return;
     }
 
-    serveWinner(bid: Buyer, sellers: Seller[]): Transaction[] {
+    serveWinner(bid: Buyer, winnerPrice: number): Transaction[] {
         let transactions: Transaction[] = [];
         let i = 0;
-
         while (bid.demand > 0) {
 
             if (bid.demand >= sellers[i].supply) {
                 bid.demand -= sellers[i].supply;
-                let transaction: Transaction = { from: sellers[i].account, to: bid.account, energy: sellers[i].supply, price: bid.price }; //Fix price.
+                let transaction: Transaction = { from: sellers[i].account, to: bid.account, energy: sellers[i].supply, price: winnerPrice }; //Fix price.
 
-                sellers.splice(i, 1); //Should drop from sellers list as there is no supply left.
+                sellers.splice(i, 1);
                 transactions.push(transaction);
                 i++;
 
             } else {
                 sellers[i].supply -= bid.demand;
-                let transaction: Transaction = { from: sellers[i].account, to: bid.account, energy: bid.demand, price: bid.price }; //Fix price.
+                let transaction: Transaction = { from: sellers[i].account, to: bid.account, energy: bid.demand, price: winnerPrice }; //Fix price.
 
                 bid.demand = 0; //To exit while-loop.
                 transactions.push(transaction);
             }
         }
-        // Drop bid from buyers
         return transactions;
     }
 
