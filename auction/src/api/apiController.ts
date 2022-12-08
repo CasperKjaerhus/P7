8c4f17012ee970fdaa13e3da0c6c227477686d81
dino <25507425+Deaxz@users.noreply.github.com>
2022-12-08 15:15:41 +0100

    Almost done with injection endpoints, need to know input encoding of keys, also some auctioneer key stuff

--- auction/src/api/apiController.ts
+++ auction/src/api/apiController.ts
@@ -6,16 +6,67 @@ import {
     Post,
     Query,
     Route,
-    //  SuccessResponse,
+    SuccessResponse,
 } from "tsoa";
-import { initialiseSPS, createEnergyTokenStorage, injectEnergy, bidOnEnergy, utiliseEnergy, startAuction, executeAuction } from "./apiService"
-import * from "../../../solana/energy-trading/target/types/"
+//import { initialiseSPS, createEnergyTokenStorage, injectEnergy, bidOnEnergy, utiliseEnergy, startAuction, executeAuction } from "./apiService"
+import { EnergyInjection } from "../../../solana/energy-trading/target/types/energy_injection"
+import * as anchor from '@project-serum/anchor';
+import { AnchorError, Program } from '@project-serum/anchor';
+import { PublicKey } from '@solana/web3.js';
 
+const provider = anchor.AnchorProvider.env()
+anchor.setProvider(provider);
+const program = anchor.workspace.EnergyInjection as Program<EnergyInjection>;
 
 @Route("api")
 export class APIController extends Controller {
 
     curAuctionID: number = 0;
+    smartPowerStoragePDA: any; // anchor.web3.PublicKey;
+
+    private async getSPSPDA() {
+
+        if (this.smartPowerStoragePDA != null) {
+            return this.smartPowerStoragePDA;
+        }
+
+        [this.smartPowerStoragePDA] = await PublicKey
+            .findProgramAddress(
+                [
+                    anchor.utils.bytes.utf8.encode("smartpowerstorage")
+                ],
+                program.programId
+            );
+
+        return this.smartPowerStoragePDA;
+    }
+
+    private errorResponse(e: any) {
+        if (e instanceof AnchorError) {
+            this.setStatus(400);
+            return e.message;
+        }
+
+        this.setStatus(500);
+        return "Something went wrong";
+    }
+
+    private keyPairFromPrivateKey(privateKey: String) {
+        //const uint8PrivateKey = Uint8Array.from(privateKey.split("").map(x => x.charCodeAt()))
+        //const uint8PrivateKey = Uint8Array.from(privateKey)
+        return anchor.web3.Keypair.fromSecretKey(uint8PrivateKey);
+    }
+
+    private async findEnergyTokenPDA(publicKey: anchor.web3.PublicKey) {
+        return PublicKey
+            .findProgramAddress(
+                [
+                    anchor.utils.bytes.utf8.encode("energytokenstorage"),
+                    publicKey.toBuffer()
+                ],
+                program.programId
+            );
+    }
 
     /**
     * Posts an injection of energy to the blockchain.
@@ -23,9 +74,24 @@ export class APIController extends Controller {
     * @param publicKey The public key of the injecting user
     * @param energySupply The amount of energy injected
     */
+    @SuccessResponse("201", "SPS successfully created")
     @Post("initSPS")
     public async initialiseSPS(): Promise<string> {
 
+        this.getSPSPDA();
+
+        try {
+            program.methods.initSps().accounts({
+                // TODO: Auctioneer skal betale for init
+                initializer: provider.wallet.publicKey,
+                smartPowerStorage: this.smartPowerStoragePDA
+            });
+        } catch (e) {
+            return this.errorResponse(e);
+        }
+
+        this.setStatus(200);
+        return "SPS intialised correctly";
     }
 
     /**
@@ -36,9 +102,27 @@ export class APIController extends Controller {
     */
     @Post("createETS")
     public async createEnergyTokenStorage(
-        @Query() publicKey: string,
+        @Query() privateKey: string,
     ): Promise<string> {
 
+        const keyPair = this.keyPairFromPrivateKey(privateKey);
+        const [energyTokenStoragePDA] = await this.findEnergyTokenPDA(keyPair.publicKey);
+
+        try {
+            let res = await program.methods
+                .createEnergyTokenStorage()
+                .accounts({
+                    prosumer: keyPair.publicKey,
+                    energyTokenStorage: energyTokenStoragePDA
+                })
+                .signers([keyPair])
+                .rpc();
+
+            return res;
+
+        } catch (e) {
+            return this.errorResponse(e);
+        }
     }
 
     /**
@@ -46,13 +130,32 @@ export class APIController extends Controller {
     * Supply public key of injecter and the amount of energy injected.
     * @param publicKey The public key of the injecting user
     * @param energySupply The amount of energy injected
+    * @isInt we would kindly ask you to provide a number here
     */
     @Post("inject")
     public async injectEnergy(
-        @Query() publicKey: string,
+        @Query() privateKey: string,
         @Query() energySupply: number
     ): Promise<string> {
 
+        const keyPair = this.keyPairFromPrivateKey(privateKey);
+        const [energyTokenStoragePDA] = await this.findEnergyTokenPDA(keyPair.publicKey);
+        this.getSPSPDA();
+
+        try {
+            let res = program.methods.sendInjection(energySupply)
+                .accounts({
+                    prosumer: keyPair.publicKey,
+                    smartPowerStorage: this.smartPowerStoragePDA,
+                    energyTokenStorage: energyTokenStoragePDA
+                })
+                .signers([keyPair])
+                .rpc();
+
+            return res;
+        } catch (e) {
+            return this.errorResponse(e);
+        }
     }
 
     /**
