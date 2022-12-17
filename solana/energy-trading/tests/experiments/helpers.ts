258ca97f8ad2e7e08ee4191456b181145edfb855
CasperKjaerhus <casperkjaerhusleth@gmail.com>
2022-12-17 09:10:55 +0100

    ExperimentzzzzzzzzZZZzzzz

--- solana/energy-trading/tests/experiments/helpers.ts
+++ solana/energy-trading/tests/experiments/helpers.ts
@@ -1,5 +1,5 @@
 import * as anchor from "@project-serum/anchor"
-import { Program } from "@project-serum/anchor";
+import { AnchorError, Program } from "@project-serum/anchor";
 import { PublicKey } from '@solana/web3.js';
 import { EnergyMarket } from "../../target/types/energy_market";
 import { findBidPDA, setupAirdropSolToKey } from "../helpers"
@@ -12,11 +12,7 @@ type accountObject = {
   ets: anchor.web3.PublicKey,
 }
 
-export function delay(ms, func): Promise<anchor.web3.TransactionSignature> {
-    return new Promise(resolve => setTimeout(() => {
-        resolve(func());
-    }, ms));
-}
+export const delay = (ms) => new Promise(r => setTimeout(r, ms));
 
 export function generateRandomInteger(max) {
     return Math.floor(Math.random() * max) + 1;
@@ -80,3 +76,58 @@ export const createProsumer = async (program, airdropSolToKey): Promise<{ prosum
 
     return {prosumer, tokenStorage};
 }
+
+
+export const sendTransaction = async (transaction: anchor.web3.Transaction, signer: anchor.web3.Keypair, connection: anchor.web3.Connection, failedAlready?: boolean): Promise<string> => {
+    const latestBlockhash = await connection.getLatestBlockhash();
+
+    transaction.lastValidBlockHeight = latestBlockhash.lastValidBlockHeight;
+    transaction.recentBlockhash = latestBlockhash.blockhash;
+    // Create a versioned Transaction out of the transaction
+    const versionedTransaction = new anchor.web3.VersionedTransaction(transaction.compileMessage())
+    versionedTransaction.sign([signer]);
+    // 
+    try {
+        const tx = await connection.sendTransaction(versionedTransaction, {
+            maxRetries: 5,
+            preflightCommitment: "finalized",
+            skipPreflight: true,
+        });
+
+        if(failedAlready)
+            console.log("Previously failed transaction has been sent");
+
+        return tx;
+
+    }catch(e) {
+
+        if(e instanceof anchor.web3.SendTransactionError){
+            if(e.message.includes("Blockhash")){
+                console.log("Transaction Failed: " + e.message + " Trying again...")
+            }
+
+            if(failedAlready){
+                console.log("it just wont work :(")
+                return undefined;
+            }
+                
+            transaction.recentBlockhash = (await connection.getLatestBlockhash()).blockhash;
+            return sendTransaction(transaction, signer, connection, true);
+        }
+    }
+}
+
+export const confirmTransaction = async (transaction: string, connection: anchor.web3.Connection, failedAlready?: boolean): Promise<anchor.web3.RpcResponseAndContext<anchor.web3.SignatureResult>> => {
+    const blockhashLatest = await connection.getLatestBlockhash();
+    try {
+        return await connection.confirmTransaction({
+            blockhash: blockhashLatest.blockhash,
+            lastValidBlockHeight: blockhashLatest.lastValidBlockHeight,
+            signature: transaction
+        });
+    }catch(e){
+        console.log("Confirmation failed: " + e);
+        return undefined;
+    }
+
+} 
--- solana/energy-trading/tests/experiments/tps.ts
+++ solana/energy-trading/tests/experiments/tps.ts
@@ -6,7 +6,7 @@ import chai from "chai";
 //import chaiAsPromised from "chai-as-promised";
 import { EnergyMarket } from "../../target/types/energy_market";
 import { findBidPDA, setupAirdropSolToKey } from "../helpers"
-import { generateRandomInteger, createInstruction, delay, createProsumer } from "./helpers"
+import { generateRandomInteger, sendTransaction, createInstruction, delay, createProsumer, confirmTransaction } from "./helpers"
 import { token } from "@project-serum/anchor/dist/cjs/utils";
 
 //chai.use(chaiAsPromised);
@@ -27,9 +27,9 @@ describe('Experiments', () => {
 
     before(async () => {
         await airdropSolToKey(consumer.publicKey, 100);
-
+        console.log("hat")
         const prosumersCreationArray: Promise<{prosumer: anchor.web3.Keypair, tokenStorage: anchor.web3.PublicKey }>[]  = []
-        for (let i = 0; i < 750; i++){
+        for (let i = 0; i < 2500; i++){
             prosumersCreationArray.push(
                 createProsumer(
                     program,
@@ -51,10 +51,7 @@ describe('Experiments', () => {
 
     });
 
-    const tpsToTest = [{
-        tps: 500,
-        duration: 20
-    }];
+    let tpsToTest = [];
     tpsToTest.forEach(({tps, duration}) => {
         it('TPS @: ' + tps, async () => {
             console.time(`TPS @: ${tps}`);
@@ -71,7 +68,7 @@ describe('Experiments', () => {
 
                 prmz.push(
                     program.methods
-                        .sendInjection(generateRandomInteger(300))
+                        .sendInjection(generateRandomInteger(1000))
                         .accounts({
                             prosumer: prosumer.publicKey,
                             smartPowerStorage: smartpowerstoragePDA,
@@ -109,59 +106,93 @@ describe('Experiments', () => {
         })
     })
 
-    const tpsToTest = [{
-        tps: 25,
-        duration: 20
+    tpsToTest = [{
+        tps: 120,
+        duration: 120
     }];
     tpsToTest.forEach(({tps, duration}) => {
         it('TPS-Scheduled @: ' + tps, async () => {
             console.time(`TPS @: ${tps}`);
             const totalTransaction = tps * duration;
-            const startTransactionCount = await provider.connection.getTransactionCount();
 
-            const start = Date.now();
-            let prmz = [];
-            console.time("Transaction Time")
+            const startTime = Date.now();
+            const prmz: {transaction: Promise<anchor.web3.Transaction>, prosumer: anchor.web3.Keypair}[] = [];
+            console.time("Transaction Creation Time")
             for(let i = 0; i < totalTransaction; i++){
-                let luckyProsumer = generateRandomInteger(1255)-1;
+                let luckyProsumer = generateRandomInteger(prosumers.length)-1;
                 let prosumer = prosumers[luckyProsumer].prosumer;
 
                 prmz.push(
-                    program.methods
-                        .sendInjection(generateRandomInteger(30))
-                        .accounts({
-                            prosumer: prosumer.publicKey,
-                            smartPowerStorage: smartpowerstoragePDA,
-                            energyTokenStorage: prosumers[luckyProsumer].tokenStorage,
-                        })
-                        .signers([prosumer])
-                        .rpc()
+                    {
+                        transaction: program.methods
+                            .sendInjection(generateRandomInteger(i))
+                            .accounts({
+                                prosumer: prosumer.publicKey,
+                                smartPowerStorage: smartpowerstoragePDA,
+                                energyTokenStorage: prosumers[luckyProsumer].tokenStorage,  
+                            })
+                            .signers([prosumer])
+                            .transaction(),
+                            prosumer: prosumer
+                    }
                 ); 
             }
 
-            let transactions = await Promise.all(prmz);
+            let transactions: {transaction: anchor.web3.Transaction, prosumer: anchor.web3.Keypair}[] = [];
 
-            console.timeEnd("Transaction Time");  
+            transactions = await Promise.all(prmz.map(async (v) => {
+                return {transaction: await v.transaction, prosumer: v.prosumer}
+            }));
 
-            let schedule = [];
-            for (transaction of transactions) {
-                schedule.push({transaction, tick: generateRandomInteger(duration*1000)});
+            console.timeEnd("Transaction Creation Time");  
+            console.time("Transaction Scheduling");
+
+            let schedules: {transaction: anchor.web3.Transaction, tick: number, prosumer: anchor.web3.Keypair}[] = [];
+            let blockhashLatest = await program.provider.connection.getLatestBlockhash();
+            for (const transaction of transactions) {
+                transaction.transaction.recentBlockhash = blockhashLatest.blockhash;
+                schedules.push({transaction: transaction.transaction, tick: generateRandomInteger(duration*1000), prosumer: transaction.prosumer});
             }
-            schedule.sort((a, b) => a.tick - b.tick);
+            schedules.sort((a, b) => a.tick - b.tick);
 
+            console.timeEnd("Transaction Scheduling");
+            console.time("Send it!");
+            const startTransactionCount = await provider.connection.getTransactionCount();
+            const transIds: Promise<string>[] = []
             let tick = 0;
-            const tickrate = 10;
-            while (tick <= duration) {
+            const tickrate = 100;
+            console.table(schedules);
+            while (tick <= duration * 1000) {
                 tick += tickrate;
-                while (schedule[0].tick <= tick) {
-                  transaction = schedule.shift();
-                  transaction.recentBlockhash = (await provider.connection.getLatestBlockhash()).blockhash;
-                  transaction.feePayer = prosumer.publicKey;
-                  anchor.web3.sendAndConfirmTransaction(provider.connection, transaction, [prosumer]);
+                while (schedules[0] != undefined && schedules[0].tick <= tick) {
+                    const schedule = schedules.shift();
+                    schedule.transaction.feePayer = schedule.prosumer.publicKey;
+                    transIds.push(sendTransaction(schedule.transaction, schedule.prosumer, provider.connection));
                 }
+                await delay(tickrate);
             }
-        )}
-    )}
+
+            const transIdsAwaited = await Promise.all(transIds);
+            const endTransactionCount = await provider.connection.getTransactionCount();
+            console.timeEnd("Send it!");
+            let totalTime = Date.now() - startTime;
+            console.log(`${endTransactionCount - startTransactionCount} in ${totalTime / 1000}s @ send-time: ${(endTransactionCount - startTransactionCount) / (totalTime / 1000)} TPS`);
+            console.time("Confirm em!");
+
+            let confirmations: Promise<anchor.web3.RpcResponseAndContext<anchor.web3.SignatureResult>>[] = []
+            for (let transaction of transIdsAwaited) {
+                confirmations.push(confirmTransaction(transaction, provider.connection));
+            }
+            await Promise.all(confirmations);
+
+            totalTime = Date.now() - startTime;
+
+            console.timeEnd("Confirm em!");
+
+            console.log(`${totalTransaction} in ${totalTime / 1000}s: ${totalTransaction / (totalTime / 1000)} TPS`);
+            console.log(`${endTransactionCount - startTransactionCount} in ${totalTime / 1000}s @ confirm-time: ${(endTransactionCount - startTransactionCount) / (totalTime / 1000)} TPS`);
+        });
+    });
 
     after(async () => {
         await program.methods.resetSmartPowerStorage().accounts({
@@ -172,5 +203,3 @@ describe('Experiments', () => {
         .rpc();
     })
 });
-
-
